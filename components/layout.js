da1555dae01cac2b684e3e740ed92a03af70c390
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-01 18:17:42 -0500

    fix impoirt issue

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,12 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Head from 'next/head'
 
 export default function Layout({ children }) {
   return (
     <>        
       <Head>
-        <title>Roshan's portfolio</title>
+        <title>Roshan&apos;s portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
