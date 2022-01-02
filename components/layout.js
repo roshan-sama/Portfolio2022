0d77c711b8503b817d2a33b0a7b47437d2b668d2
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-01 21:32:08 -0500

    edit layout add canta wrapper

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,30 @@
-import Navbar from "./navbar";
-import Footer from "./footer";
-import Head from 'next/head'
+import Head from "next/head";
+import { AppShell, Card, Header, MantineProvider } from "@mantine/core";
+import VantaWrapper from "./vanta-wrapper";
 
 export default function Layout({ children }) {
   return (
-    <>        
+    <>
       <Head>
         <title>Roshan&apos;s portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
+      <VantaWrapper>
+        <MantineProvider theme={{ colorScheme: "dark" }}>
+          <AppShell
+            padding="md"
+            header={
+              <Header height={60} padding="xs">
+                {/* Header content */}
+              </Header>
+            }
+          >
+            <main>
+              <Card>{children}</Card>
+            </main>
+          </AppShell>
+        </MantineProvider>
+      </VantaWrapper>
     </>
   );
 }
---
+++ components/vanta-wrapper.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import Head from 'next/head';
+
+export default function VantaWrapper({ children }) {
+  const vantaWrapperId = "vanta-background-div";
+  useEffect(() => {
+    document.getElementById(vantaWrapperId).style.position = "fixed";
+    document.getElementById(vantaWrapperId).style.height = "100vh";
+    document.getElementById(vantaWrapperId).style.width = "100vw";
+    VANTA.NET({
+      el: `#${vantaWrapperId}`,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0xffb84e,
+      backgroundColor: 0xd0025,
+      points: 12.0,
+      spacing: 17.0,
+    });
+  });
+  return (
+    <>
+      <Head>
+        <script type="text/javascript" src="three.r119.min.js"></script>
+        <script type="text/javascript" src="vanta.net.min.js"></script>
+      </Head>
+      <div id={vantaWrapperId}>{children}</div>
+    </>
+  );
+}
