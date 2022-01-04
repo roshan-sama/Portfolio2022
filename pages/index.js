f285986b991fa9288e6efe46622eb855da6e44f0
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-03 22:32:05 -0500

    add componetns to index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import { Card, Center, Grid, Container } from "@mantine/core";
 import Layout from "../components/layout";
 
 export default function Home() {
-  return <>Test div</>;
+  return (
+    <Container fluid={true} style={{height: "inherit", marginTop: "-60px"}}>
+      <Grid justify="center" align="center" style={{height: "inherit"}}>
+        <Grid.Col span={12} sm={8} lg={6}>
+          <Card>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+          </Card>
+        </Grid.Col>
+      </Grid>
+    </Container>
+  );
 }
+
 Home.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
