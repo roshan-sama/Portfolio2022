bbd59060d44f6cb2071e1c5cff78c54b66adce60
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-04 14:39:45 -0500

    preview career item

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import { Card, Center, Grid, Container } from "@mantine/core";
 import Layout from "../components/layout";
+import CareerItem from "../components/Career/career-item";
 
 export default function Home() {
   return (
-    <Container fluid={true} style={{height: "inherit", marginTop: "-60px"}}>
-      <Grid justify="center" align="center" style={{height: "inherit"}}>
+    <Container fluid={true} style={{ height: "inherit", marginTop: "-60px" }}>
+      <Grid justify="center" align="center" style={{ height: "inherit" }}>
         <Grid.Col span={12} sm={8} lg={6}>
           <Card>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
+            <CareerItem
+              career={{
+                id: "1",
+                companyName: "Epic",
+                imgUrl: "img/epiclogo.jpg",
+                description: "some desc",
+                startDate: new Date(),
+              }}
+            />
           </Card>
         </Grid.Col>
       </Grid>
