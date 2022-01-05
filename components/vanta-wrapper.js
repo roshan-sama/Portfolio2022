ef5e012b120f3a7f845ce839aed74e274eaaa96e
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-04 21:30:37 -0500

    defer script loading

--- components/vanta-wrapper.js
+++ components/vanta-wrapper.js
@@ -3,37 +3,41 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Vanta.module.css";
 import { useReducedMotion } from "@mantine/hooks";
-import bg from '../public/img/vantabg_static.png'
+import bg from "../public/img/vantabg_static.png";
 
 export default function VantaWrapper({ children }) {
   const reduceMotion = useReducedMotion();
   const vantaWrapperId = "vanta-background-div";
 
+  const initVanta = async () => {
+    VANTA.NET({
+      el: `#${vantaWrapperId}`,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      speed: 1,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0xffb84e,
+      backgroundColor: 0xd0025,
+      points: 12.0,
+      spacing: 17.0,
+    });
+  };
+
   useEffect(() => {
     if (reduceMotion === false) {
-      VANTA.NET({
-        el: `#${vantaWrapperId}`,
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.0,
-        minWidth: 200.0,
-        speed: 1,
-        scale: 1.0,
-        scaleMobile: 1.0,
-        color: 0xffb84e,
-        backgroundColor: 0xd0025,
-        points: 12.0,
-        spacing: 17.0,
-      });
+      initVanta()
     }
   }, [reduceMotion]);
 
   return (
     <>
       <Head>
-        <script type="text/javascript" src="three.r119.min.js"></script>
-        <script type="text/javascript" src="vanta.net.min.js"></script>
+        <script defer type="text/javascript" src="three.r119.min.js"></script>
+        <script defer type="text/javascript" src="vanta.net.min.js"></script>
       </Head>
       {!reduceMotion && (
         <div id={vantaWrapperId} className={styles.backgroundfill}>
@@ -42,10 +46,16 @@ export default function VantaWrapper({ children }) {
       )}
       {reduceMotion && (
         <>
-          <div key={"background"} style={{ zIndex: -5 }} className={styles.backgroundfill}>
-            <Image src={bg} layout="fill" />
+          <div
+            key={"background"}
+            style={{ zIndex: -5 }}
+            className={styles.backgroundfill}
+          >
+            <Image alt="Vanta Net background" src={bg} layout="fill" />
           </div>
-          <div key={"childrenelems"}  style={{ zIndex: 0 }}>{children}</div>
+          <div key={"childrenelems"} style={{ zIndex: 0 }}>
+            {children}
+          </div>
         </>
       )}
     </>
