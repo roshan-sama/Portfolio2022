5cdd9b59574956fff9cfae6f9e1c46172463be16
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-05 15:40:17 -0500

    fix spaciong

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
-import { Card, Center, Grid, Container } from "@mantine/core";
+import { Card, Center, Grid, Container, Blockquote } from "@mantine/core";
 import Layout from "../components/layout";
 import CareerItem from "../components/Career/career-item";
 
 export default function Home() {
   return (
-    <Container fluid={true} style={{ height: "inherit", marginTop: "-60px" }}>
+    <Container fluid={true} style={{ height: "inherit" }}>
       <Grid justify="center" align="center" style={{ height: "inherit" }}>
-        <Grid.Col span={12} sm={8} lg={6}>
+        {/* TODO: Make that number 60 a constant defined in a different file. Its the same size as the header height */}
+        <Grid.Col span={12} sm={8} lg={6} style={{ marginTop: -60 }}>
+          <Grid>
+            <Grid.Col span={6} >
+              <Card >
+                <Blockquote>
+                  The harder you work for something, the greater you'll feel
+                  when you achieve it
+                </Blockquote>
+              </Card>
+            </Grid.Col>
+            <Grid.Col span={6} offset={6}>
+              <Card style={{ margin: "3rem" }}>
+                <Blockquote>
+                  Especially if its troubleshooting a generic error
+                  message for two hours
+                </Blockquote>
+              </Card>
+            </Grid.Col>
+          </Grid>
           <Card>
             <CareerItem
               career={{
