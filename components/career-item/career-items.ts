c7b0fb36c8b92769edb361c39c0c0457925c5dcb
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-07 09:41:02 -0500

    rename career to career item

---
+++ components/career-item/career-items.ts
@@ -0,0 +1,9 @@
+import CareerItemType from "./career-item-type";
+
+const CareerItems: CareerItemType[] = [
+    {
+        id: "id_citem_syndops",
+        companyid: "id_comp_synergy",
+        description: ""
+    }
+]
