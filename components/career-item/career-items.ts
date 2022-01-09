6afca4bfd7dc14a3eff4b719d607acd2ea3273ed
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-08 19:42:24 -0500

    remove tsx file, add career items

--- components/career-item/career-items.ts
+++ components/career-item/career-items.ts
@@ -1,9 +1,20 @@
 import CareerItemType from "./career-item-type";
 
 const CareerItems: CareerItemType[] = [
-    {
-        id: "id_citem_syndops",
-        companyid: "id_comp_synergy",
-        description: ""
-    }
-]
+  {
+    id: "id_citem_syndops",
+    companyId: "id_comp_synergy",
+    description: "",
+    startDate: new Date(),
+    roleName: "Devops Engineer",
+  },
+  {
+    id: "id_citem_synfstack",
+    companyId: "id_comp_synergy",
+    description: "",
+    startDate: new Date(),
+    roleName: "Fullstack Engineer",
+  },
+];
+
+export default CareerItems;
