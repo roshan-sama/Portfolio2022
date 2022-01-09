7bf3f21d8418d21b65ff3a653c6490c216ac3165
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-09 00:55:27 -0500

    display about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Card, Center, Grid, Container, Blockquote } from "@mantine/core";
 import Layout from "../components/layout";
 import CareerItem from "../components/career-item/career-item";
 import CompaniesTable from "../components/Company/companies-table";
+import AboutMe from "../components/about-me";
 
 export default function Home() {
   return (
@@ -10,11 +11,12 @@ export default function Home() {
         <Grid.Col span={12} sm={8} lg={6}>
           <Grid justify="center">
             <Grid.Col span={12} sm={6}>
-              <Card style={{ margin: "0.5rem" }}>
+              <AboutMe />
+              {/* <Card style={{ margin: "0.5rem" }}>
                 <Blockquote cite="Steve Wozniak">
                   Never trust a computer you can&apos;t throw out a window.
                 </Blockquote>
-              </Card>
+              </Card> */}
             </Grid.Col>
           </Grid>
           <CompaniesTable />
