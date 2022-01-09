4eab1ea54cf6709780be5f6a30e692ca6fe22207
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-09 00:55:18 -0500

    display name in title

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,17 +10,16 @@ import VantaWrapper from "./vanta-wrapper";
 import { ColorScheme } from "../types";
 
 export default function Layout({ children }) {
-
+  
   const [value, setValue] = useLocalStorageValue<ColorScheme>({
     key: 'color-scheme',
     defaultValue: 'dark',
   });
-
+  
   return (
     <>
       <Head>
-        <title>Roshan&apos;s portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{process.env.NEXT_PUBLIC_NAME}&apos;s portfolio</title>
         <script defer type="text/javascript" src="three.r119.min.js"></script>
         <script defer type="text/javascript" src="vanta.net.min.js"></script>
       </Head>
@@ -30,7 +29,7 @@ export default function Layout({ children }) {
             padding="md"   
             styles={(theme) => ({
               main: { height: "100vh" },
-            })}         
+            })}
             header={
               <Header height={60} padding="xs">
                 {/* Header content */}
