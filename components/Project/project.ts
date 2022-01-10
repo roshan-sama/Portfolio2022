702a4bfb0036a509237d73b74e87f9f92ca4c4e2
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-09 19:14:38 -0500

    add skill array to project

--- components/Project/project.ts
+++ components/Project/project.ts
@@ -1,7 +1,10 @@
+import Skill from "../Skill/skill";
+
 interface Project{
     id: string,
     name: string,
     description: string, // markdown?
+    skills: Skill[]
 }
 
 export default Project
