83b97ff9ad095f1a518dea7f13f0a778c5a7c475
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-10 02:25:07 -0500

    add skills items

---
+++ components/Skill/skills.ts
@@ -0,0 +1,71 @@
+import Skill from "./skill";
+
+const skills: Skill[] = [
+  {
+    name: "C#",
+    description: "Object-oriented programming language by Microsoft",
+    infoUrl: "https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/",
+  },
+  {
+    name: "Java",
+    description: "Popular object-oriented programming language by Oracle",
+    infoUrl: "https://www.oracle.com/java/",
+  },
+  {
+    name: "Javascript",
+    description: "Popular webpage scripting language by Ecma international",
+    infoUrl: "https://www.ecma-international.org/technical-committees/tc39/",
+  },
+  {
+    name: "React",
+    description: "JavaScript library for building user interfaces by Facebook",
+    infoUrl: "https://reactjs.org/",
+  },
+  {
+    name: "NextJS",
+    description: "React Framework  webpages by Vercel",
+    infoUrl: "https://nextjs.org/",
+  },
+  {
+    name: "BlitzJS",
+    description: "Fullstack React Framework, inspired by Ruby on Rails",
+    infoUrl: "https://blitzjs.com/",
+  },
+  {
+    name: "Salesforce",
+    description: "Low code/ no code CRM platform",
+    infoUrl: "https://www.salesforce.com/",
+  },
+  {
+    name: "Git",
+    description: "Open source version control system",
+    infoUrl: "https://git-scm.com/",
+  },
+  {
+    name: "Docker",
+    description: "Software tool that creates and manages images and containers",
+    infoUrl: "https://www.docker.com/",
+  },
+  {
+    name: "Kubernetes",
+    description: "Open source system for automating management of containerized applications by the CNCF",
+    infoUrl: "https://www.salesforce.com/",
+  },
+  {
+    name: "Helm",
+    description: "Package manager for Kubernetes by the CNCF",
+    infoUrl: "https://helm.sh/",
+  },
+  {
+    name: "ArgoCD",
+    description: "Open source system for automating management of containerized applications by the CNCF",
+    infoUrl: "https://argoproj.github.io/cd/",
+  },
+  {
+    name: "Harbor",
+    description: "Open source container and artifact registry by the CNCF",
+    infoUrl: "https://goharbor.io/",
+  },
+];
+
+export default Skill;
