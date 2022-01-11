225c320e5a3c181f0171351c685167b038347dbc
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-10 22:21:25 -0500

    add blogs

---
+++ components/blog/blogs.ts
@@ -0,0 +1,16 @@
+const blogs = [
+    {
+        title: "How this portfolio app was created",
+        items: "Describe: 1) The components used in the app. 2) Why the components were chosen \
+        3) challenges you ran into when creating the app and how they were resolved"
+    },
+    {
+        title: "Blitzjs subtemplate contribution",
+        items: "Describe: 1) The additions made to the feature 2) Why the specific additions were made \
+        3) challenges you ran into when creating working on the feature"
+    }
+    // Basically, any project I worked on can be blogged about in the above components/ skills used, why those were chosen, and
+    // challenges encountered. Each of the challenges encountered could get their own sub blogs if they are large enough
+]
+
+export default blogs
