02d5feb237ec7d758ae444c06f7d665d932b0939
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-12 13:25:37 -0500

    use media queries to adjust position around name

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,29 @@ import {
   Container,
   Blockquote,
   ScrollArea,
+  MediaQuery,
 } from "@mantine/core";
 import Layout from "../components/layout";
 import CareerItem from "../components/career-item/career-item";
 import CompaniesTable from "../components/Company/companies-table";
 import AboutMe from "../components/about-me";
+import { useMediaQuery } from "@mantine/hooks";
 
 export default function Home() {
+  const large = useMediaQuery("(min-width: 900px)");
   return (
-    <Container fluid={true} style={{ height: "80vh" }}>
+    <Container fluid={true} style={{ marginBottom: "20vh" }}>
       <Grid justify="center" align="start" style={{ height: "inherit" }}>
         <Grid.Col span={12} sm={8} lg={6}>
           <Grid justify="center">
-            <Grid.Col span={12} sm={6}>
+            <Grid.Col span={12} md={6} offset={large ? 6 : 0}>
+              {/* // TODO: Make height difference below inversely proportional to view height
+Smaller device heights must make the spacing larger
+*/}
+
+              <MediaQuery query="(max-width: 965px)" styles={{ height: "6vw" }}>
+                <div style={{}}></div>
+              </MediaQuery>
               <AboutMe />
               {/* <Card style={{ margin: "0.5rem" }}>
                 <Blockquote cite="Steve Wozniak">
