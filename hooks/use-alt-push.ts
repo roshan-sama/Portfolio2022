a7b02ccdb7e53302dd94973198fa5eb3785574d0
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-13 21:27:01 -0500

    create push fn that preserves query keys

---
+++ hooks/use-alt-push.ts
@@ -0,0 +1,30 @@
+import { NextRouter } from "next/router";
+
+const preservedKeys = ["path"];
+
+const preservedPush: (
+  router: NextRouter,
+  path: string,
+  addedQueries?: any,
+  as?: any,
+  options?: any
+) => Promise<boolean> = async (router, path, addedQueries, as, options) => {
+  let querystr = { ...addedQueries };
+
+  preservedKeys.forEach((key) => {
+    if (router.query[key] && router.query[key].length > 0) {
+      querystr[key] = router.query[key];
+    }
+  });
+
+  return router.push(
+    {
+      pathname: path,
+      query: querystr,
+    },
+    as,
+    options
+  );
+};
+
+export default preservedPush;
