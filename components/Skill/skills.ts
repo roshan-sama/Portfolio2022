e64ead45fa2123193e97fce78d93769f3fdc40bf
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-14 22:58:47 -0500

    add ids and export skill

--- components/Skill/skills.ts
+++ components/Skill/skills.ts
@@ -1,71 +1,104 @@
 import Skill from "./skill";
 
-const skills: Skill[] = [
+const Skills: Skill[] = [
   {
+    id: "id_skill_csharp",
     name: "C#",
     description: "Object-oriented programming language by Microsoft",
     infoUrl: "https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/",
+    skillCategoryId: "id_skcat_language"
   },
   {
+    id: "id_skill_java",
     name: "Java",
     description: "Popular object-oriented programming language by Oracle",
     infoUrl: "https://www.oracle.com/java/",
+    skillCategoryId: "id_skcat_language"
   },
   {
+    id: "id_skill_js",
     name: "Javascript",
     description: "Popular webpage scripting language by Ecma international",
     infoUrl: "https://www.ecma-international.org/technical-committees/tc39/",
+    skillCategoryId: "id_skcat_language"
   },
   {
+    id: "id_skill_react",
     name: "React",
     description: "JavaScript library for building user interfaces by Facebook",
     infoUrl: "https://reactjs.org/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_nextjs",
     name: "NextJS",
     description: "React Framework  webpages by Vercel",
     infoUrl: "https://nextjs.org/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_blitzjs",
     name: "BlitzJS",
     description: "Fullstack React Framework, inspired by Ruby on Rails",
     infoUrl: "https://blitzjs.com/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_salesforce",
     name: "Salesforce",
     description: "Low code/ no code CRM platform",
     infoUrl: "https://www.salesforce.com/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_git",
     name: "Git",
     description: "Open source version control system",
     infoUrl: "https://git-scm.com/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_docker",
     name: "Docker",
     description: "Software tool that creates and manages images and containers",
     infoUrl: "https://www.docker.com/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_microsvcs",
+    name: "Microservices",
+    description: "Software architectures pattern created to develop and scale applications quicker.",
+    infoUrl: "https://en.wikipedia.org/wiki/Microservices",
+    skillCategoryId: "id_skcat_concepts"
+  },
+  {
+    id: "id_skill_k8s",
     name: "Kubernetes",
     description: "Open source system for automating management of containerized applications by the CNCF",
     infoUrl: "https://www.salesforce.com/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_helm",
     name: "Helm",
     description: "Package manager for Kubernetes by the CNCF",
     infoUrl: "https://helm.sh/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_argocd",
     name: "ArgoCD",
     description: "Open source system for automating management of containerized applications by the CNCF",
     infoUrl: "https://argoproj.github.io/cd/",
+    skillCategoryId: "id_skcat_tool"
   },
   {
+    id: "id_skill_harbor",
     name: "Harbor",
     description: "Open source container and artifact registry by the CNCF",
     infoUrl: "https://goharbor.io/",
+    skillCategoryId: "id_skcat_tool"
   },
 ];
 
-export default Skill;
+export default Skills;
