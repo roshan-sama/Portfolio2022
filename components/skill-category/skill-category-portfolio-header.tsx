abdf108662bd94a00c9ffad7050f8c534ab033fc
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-14 22:59:07 -0500

    add skill chips

--- components/skill-category/skill-category-portfolio-header.tsx
+++ components/skill-category/skill-category-portfolio-header.tsx
@@ -1,14 +1,48 @@
-import { Grid } from "@mantine/core";
+import { Card, Chip, Chips, Divider, Grid } from "@mantine/core";
+import { useMemo, useState } from "react";
+import Skills from "../Skill/skills";
 import SkillCategoryType from "./skill-category-type";
 
-const PortfolioRoleHeader: React.FC<{ skillCategory: SkillCategoryType }> = ({
+export function ColWrapper(props: React.ComponentProps<typeof Grid.Col>) {
+  return (
+    <Grid.Col {...props} span={props.span} style={{ ...props.style }}>
+      {props.children}
+    </Grid.Col>
+  );
+}
+
+const PortfolioSkillHeader: React.FC<{ skillCategory: SkillCategoryType }> = ({
   skillCategory,
 }) => {
+  const { skills, skillIds } = useMemo(() => {
+    const skills = Skills.filter(
+      (skill) => skill.skillCategoryId === skillCategory.id
+    );
+    const skillIds = skills.map((skill) => skill.id);
+
+    return {
+      skills,
+      skillIds,
+    };
+  }, [skillCategory]);
+
+  // const [selectedSkills, setSelectedSkills] = useState(skillIds)
+
   return (
-    <Grid.Col span={12} sm={4}>
+    <Card shadow="lg" padding="sm">
       {skillCategory.name}
-    </Grid.Col>
+      <Divider my="xs" />
+      <Chips multiple value={skillIds}>
+        {skills.map((skill) => {
+          return (
+            <Chip key={skill.id} value={skill.id}>
+              {skill.name}
+            </Chip>
+          );
+        })}
+      </Chips>
+    </Card>
   );
 };
 
-export default PortfolioRoleHeader;
+export default PortfolioSkillHeader;
