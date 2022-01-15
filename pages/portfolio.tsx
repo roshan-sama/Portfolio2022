6a3f51cd592ac8f190c1aea6f60b06b072cfb6d8
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-14 22:59:58 -0500

    display skill categories dynsmically based on role chosen

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -13,6 +13,13 @@ import CareerItem from "../components/career-item/career-item";
 import { useRouter } from "next/router";
 import Roles from "../components/role/roles";
 import RoleType from "../components/role/role-type";
+import SkillCategories from "../components/skill-category/skillCategories";
+import PortfolioSkillHeader, {
+  ColWrapper,
+} from "../components/skill-category/skill-category-portfolio-header";
+import Skill from "../components/Skill/skill";
+import SkillCategoryType from "../components/skill-category/skill-category-type";
+import { useEffect, useMemo, useState } from "react";
 
 const getRoleById = (roleid: string): RoleType =>
   Roles.find((role) => role.id === `id_role_${roleid}`);
@@ -33,32 +40,102 @@ const getPortfolioName = (id: string | string[]) => {
   }
 };
 
+const getCategoryById = (id: string): SkillCategoryType => {
+  return SkillCategories.find((skill) => skill.id === id);
+};
+
+const getRoles = (id: string | string[]): RoleType[] => {
+  if (!id) {
+    return undefined;
+  }
+
+  if (isStringArray(id)) {
+    return id.map((id) => getRoleById(id));
+  } else {
+    return [getRoleById(id)];
+  }
+};
+
+const getDeduplicatedSkills = (
+  skills: Skill[]
+): { dedupSkills: Skill[]; dedupCategories: SkillCategoryType[] } => {
+  const seenSkillIds = {};
+  const seenCategoryIds = {};
+  const dedupSkills = [];
+  const dedupCategories = [];
+
+  skills.forEach((skill) => {
+    if (seenSkillIds[skill.id] === skill.id) {
+      return;
+    }
+    dedupSkills.push(skill);
+    seenSkillIds[skill.id] = skill.id;
+    if (seenCategoryIds[skill.skillCategoryId] === skill.skillCategoryId) {
+      return;
+    }
+    dedupCategories.push(getCategoryById(skill.skillCategoryId));
+    seenCategoryIds[skill.skillCategoryId] = skill.skillCategoryId;
+  });
+
+  return { dedupSkills, dedupCategories };
+};
+
 export default function Portfolio() {
   const router = useRouter();
-  const { role } = router.query;
+  const { roleId } = router.query;
 
   const theme = useMantineTheme();
   const secondaryColor =
     theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2];
 
+  const [roles, setRoles] = useState([]);
+
+  useEffect(() => {
+    let roles = getRoles(roleId);
+    if (roles === undefined || roles[0] === undefined) {
+      roles = Roles;
+    }
+    setRoles((prev) => [...roles]);
+  }, [roleId]);
+
+  const { dedupSkills, dedupCategories } = useMemo(() => {
+    const skills = roles.flatMap((role) => role.relevantSkills);
+    return getDeduplicatedSkills(skills);
+  }, [roles, getDeduplicatedSkills]);
+
+  const numSkillCats = dedupCategories.length;
+  const adjustedColSpan =
+    dedupCategories.length >= 1 && dedupCategories.length <= 3
+      ? 12 / numSkillCats
+      : 12;
+
   return (
     <Container fluid={true} style={{ marginTop: "50vh" }}>
-      <Grid justify="center" align="center" style={{ height: "inherit" }}>
+      <Grid
+        gutter="xs"
+        justify="center"
+        align="center"
+        style={{ height: "inherit" }}
+      >
         <Grid.Col span={12} md={10} xl={8}>
-          <Card padding="sm">
-            <Title order={1}>Roshan&apos;s {getPortfolioName(role)}</Title>
-            <Card style={{ backgroundColor: secondaryColor }}>
+          <Card padding="xs">
+            <Title order={1} style={{ marginBottom: "1rem" }}>
+              Roshan&apos;s {getPortfolioName(roleId)}
+            </Title>
+            <Card
+              style={{ backgroundColor: secondaryColor, backgroundImage: "" }}
+            >
               <Container fluid>
-                <Grid>
-                  <Grid.Col span={12} sm={4}>
-                    a
-                  </Grid.Col>
-                  <Grid.Col span={12} sm={4}>
-                    a
-                  </Grid.Col>
-                  <Grid.Col span={12} sm={4}>
-                    a
-                  </Grid.Col>
+                <Grid gutter="xs">
+                  {dedupCategories.map((category) => (
+                    <ColWrapper
+                      key={category.id}
+                      span={12}
+                      sm={adjustedColSpan}
+                    >
+                      <PortfolioSkillHeader skillCategory={category} />
+                    </ColWrapper>
+                  ))}
                 </Grid>
               </Container>
             </Card>
