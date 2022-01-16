d50e3e8fbc0d7e5c814bed8ebb8c2829d57931d0
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-16 14:08:54 -0500

    add change roles button

---
+++ components/change-role-btn.tsx
@@ -0,0 +1,36 @@
+import { MultiSelect } from "@mantine/core";
+import { useRouter } from "next/router";
+import preservedPush from "../hooks/use-alt-push";
+import swapPush from "../hooks/use-update-push";
+import getRolesById from "../utils/get-roles-by-ids";
+import Roles from "./role/roles";
+
+const ChangeRoleBtn = () => {
+  const router = useRouter();
+  const { roleId } = router.query;
+
+  const data = Roles.map((role) => ({
+    value: role.id.split("id_role_")[1],
+    label: role.name,
+  }));
+
+  const roles = getRolesById(roleId) ?? [];
+  const selectedValues = roles.map((role) => {
+    return role ? role.id.split("id_role_")[1] : ""
+  })
+
+  return (
+    <MultiSelect
+      label="Change Role"
+      placeholder="Pick one"
+      data={data}
+      value={selectedValues}
+      style={{ marginBottom: "1rem", minWidth: "15rem" }}
+      onChange={(ids) => {
+        swapPush(router, ids)
+      }}
+    />
+  );
+};
+
+export default ChangeRoleBtn;
