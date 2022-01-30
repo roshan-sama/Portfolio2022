988256a7b2255e2fb70c825bdf2afaa9696f070a
Roshan Manuel <Roshan,manuel@angelic-group.com>
2022-01-29 19:59:20 -0500

    handle not found files

--- components/blog/blog.tsx
+++ components/blog/blog.tsx
@@ -3,18 +3,22 @@ import { useEffect, useState } from "react";
 import purify from "dompurify/dist/purify";
 import { parse } from "marked";
 
-const Blog: React.FC<{ id?: string }> = ({ id }) => {
+const Blog: React.FC<{ path: string }> = ({ path }) => {
   const [loading, setLoading] = useState(true);
+  const [notFound, setNotFound] = useState(false);
   const markDownElementId = "mark-down-root-element";
-  
+
   useEffect(() => {
-    if (loading) {
-      getMarkDown();
-    }
-  }, [loading]);
+    getMarkDown();
+  }, [path]);
 
   const getMarkDown = async () => {
-    const md = await fetch("posts/test.md");
+    const md = await fetch(path);
+    if (md.status >= 400) {
+      setNotFound(true);
+      setLoading(false);
+      return;
+    }
     const markdownHtml = parse(await md.text());
     const sanitizedHtml = purify.sanitize(markdownHtml);
     // DO NOT ALTER THE HTML AFTER THE SANITIZATION STEP ABOVE
@@ -38,7 +42,12 @@ const Blog: React.FC<{ id?: string }> = ({ id }) => {
           <Loader />
         </Center>
       )}
-      <div style={{margin: "32px"}} id={markDownElementId}></div>
+      {notFound && (
+        <div style={{ margin: "32px" }}>
+          The requested Blog post was not found
+        </div>
+      )}
+      <div style={{ margin: "32px" }} id={markDownElementId}></div>
     </Card>
   );
 };
